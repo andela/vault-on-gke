3633c97d9eaa6eba3fc83b1262b40a5eb36b835a
Olatunde Alex-Oni <tunde.alexoni@yahoo.com>
2019-01-15 21:02:02 +0100

    ft(backend): setup remote backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,5 @@
+# bucket and project value should match resources in admin project
+# TODO: ensure single source truth to ensure bucket and project values are consistent
 terraform {
  backend "gcs" {
    bucket  = "andela-tech-infra-admin"
